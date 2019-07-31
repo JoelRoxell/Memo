70d65022cda52ad811b35bd69a11471e7bcbfcdf
Joel Roxell <joel.roxell@annevo.se>
2019-07-31 10:43:00 +0200

    Account: added sub-root animation.

--- src/components/App/subs/Account/Account.tsx
+++ src/components/App/subs/Account/Account.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
-import { Switch, Route } from 'react-router'
+import { Switch, Redirect, Route } from 'react-router-dom'
 
 import * as style from './Account.scss'
 import { UserContext } from 'contexts/user-context'
 import Dashboard from './subs/Dashboard'
 import Navigation from './subs/Navigation'
+import posed, { PoseGroup } from 'react-pose'
+import Loader from 'components/common/Loader'
+
+const RouteContainer = posed.div({
+  enter: { opacity: 1, delay: 100, beforeChildren: true },
+  exit: { opacity: 0 }
+})
 
 function Account() {
   const user = React.useContext(UserContext) as UserContext
@@ -21,17 +28,43 @@ function Account() {
         <Navigation user={user} />
 
         <div className={style.view}>
-          <Switch>
-            <Route
-              exact
-              path="/account/dashboard"
-              render={() => (
-                <>
-                  <Dashboard />
-                </>
-              )}
-            />
-          </Switch>
+          <Route
+            render={({ location }) => {
+              const location_key = location.pathname.split('/')[2] // only animate view on root-sub(2)-path-changes.
+
+              return (
+                <PoseGroup>
+                  <RouteContainer key={location_key} style={{ width: '100%' }}>
+                    <React.Suspense fallback={<Loader active key="loader" />}>
+                      <Switch location={location} key="switcher">
+                        <Route
+                          exact
+                          path="/account/dashboard"
+                          render={() => (
+                            <>
+                              <Dashboard />
+                            </>
+                          )}
+                        />
+
+                        <Route
+                          exact
+                          path="/account/grow-zones"
+                          render={() => (
+                            <>
+                              <div>Grow some</div>
+                            </>
+                          )}
+                        />
+
+                        <Route exact path="/" render={() => <Redirect to="/login" key="none" />} />
+                      </Switch>
+                    </React.Suspense>
+                  </RouteContainer>
+                </PoseGroup>
+              )
+            }}
+          />
         </div>
       </div>
     </div>
